dd7ae618e6e9de1244a8c34faecf98e1cc5d4ef5
shaunmcbean <gshivank7@gmail.com>
2025-04-05 16:21:11 -0400

    Initial Commit

---
+++ healthHAND/healthHANDApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthHANDApp.swift
+//  healthHAND
+//
+//  Created by Shivank Gupta on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct healthHANDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ healthHANDTests/healthHANDTests.swift
@@ -0,0 +1,17 @@
+//
+//  healthHANDTests.swift
+//  healthHANDTests
+//
+//  Created by Shivank Gupta on 4/5/25.
+//
+
+import Testing
+@testable import healthHAND
+
+struct healthHANDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
